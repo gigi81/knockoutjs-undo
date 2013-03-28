af792f352d5b28aa7b35b6791b8948a71b2ede3a
Luigi Grilli <luigi.grilli@gmail.com>
2013-03-28 11:28:47 +0000

    Fix for Array.isArray
    Fix for Array.isArray as not supported by older browsers

--- knockout.undo.js
+++ knockout.undo.js
@@ -1,3 +1,9 @@
+if(!Array.isArray) {
+	Array.isArray = function (vArg) {
+		return Object.prototype.toString.call(vArg) === "[object Array]";
+	};
+}
+
 ko.undoService = initUndoService();
 
 function undoStack() {
